8550419ec107ce0bfa4099662e22f9db3833b377
Yuki Kubota <rakudalms@gmail.com>
2021-05-31 09:57:44 +0900

    株一覧ページのスクレイピング（単一ページ）

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,16 @@
 class Stock < ApplicationRecord
   validates :ticker, :company, :url, presence: true
 
+  require 'mechanize'
+
   def self.stock_urls
-    links = []
-    agent = Stock.new
-    
+    agent = Mechanize.new
+    current_page = agent.get("https://finviz.com/screener.ashx")
+    tickers = current_page.search(".screener-link-primary").map {|ticker| ticker.text }
+    urls = current_page.search(".screener-body-table-nw a").map { |url| url.get_attribute('href') }.uniq
+
+    tickers.zip(urls) do |ticker, url|
+      puts "#{ticker}: https://finviz.com/#{url}"
+    end
   end
 end
