7156e6008d2697a4b210e5248420df00351628e5
Yuki Kubota <rakudalms@gmail.com>
2021-05-31 12:51:41 +0900

    株一覧ページのスクレイピング（複数ページ）改良中

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,14 +3,29 @@ class Stock < ApplicationRecord
 
   require 'mechanize'
 
-  def self.stock_urls
+  # def self.stock_urls
     agent = Mechanize.new
-    current_page = agent.get("https://finviz.com/screener.ashx")
-    tickers = current_page.search(".screener-link-primary").map {|ticker| ticker.text }
-    urls = current_page.search(".screener-body-table-nw a").map { |url| url.get_attribute('href') }.uniq
+    next_url = ""
+    next_url_array = []
 
-    tickers.zip(urls) do |ticker, url|
-      puts "#{ticker}: https://finviz.com/#{url}"
+    while true do
+      current_page = agent.get("https://finviz.com/" + next_url)
+      tickers = current_page.search(".screener-link-primary").map {|ticker| ticker.text }
+      urls = current_page.search(".screener-body-table-nw a").map { |url| url.get_attribute('href') }.uniq
+
+      tickers.zip(urls) do |ticker, url|
+        puts "#{ticker}: https://finviz.com/#{url}"
+      end
+
+      next_link = current_page.at(".screener_pagination a:last-child")
+      if next_link.text == "next"
+        next_url_array = next_link.get_attribute('href')
+      else
+        next_url_array = []
+      end
+
+      next_url = next_url_array.join
+
+      break unless next_url
     end
-  end
-end
+# end
