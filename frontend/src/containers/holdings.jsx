beb2c11c2fb5f1dbbbd515503619d7693c50864b
Yuki Kubota <rakudalms@gmail.com>
2021-06-18 14:30:03 +0900

    カバーイメージ（仮）を表示

--- frontend/src/containers/holdings.jsx
+++ frontend/src/containers/holdings.jsx
@@ -1,7 +1,31 @@
 import React, { Fragment, useEffect } from 'react';
+import styled from 'styled-components';
 
 //apis
 import { fetchHoldings } from '../apis/holdings';
+
+//images
+import MainLogo from '../images/logo.jpg';
+import MainCover from '../images/cover.jpg';
+
+//css
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCoverImage = styled.img`
+  height: 400px;
+`;
 
 export const Holdings = ({ match }) => {
   useEffect(() => {
@@ -12,6 +36,12 @@ export const Holdings = ({ match }) => {
   },[])
   return(
     <Fragment>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverWrapper>
+        <MainCoverImage src={MainCover} alt="main cover" />
+      </MainCoverWrapper>
       保有株一覧
       <p>
         UserIDは {match.params.user_id} です
